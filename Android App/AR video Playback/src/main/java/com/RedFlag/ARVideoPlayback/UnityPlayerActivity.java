5c611ee77633b2381527621dcc3ce8b81235e6bf
Fardeen Khan <khanfardeen9983@gmail.com>
2018-09-18 00:45:33 +0530

    Initial commit

---
+++ Android App/AR video Playback/src/main/java/com/RedFlag/ARVideoPlayback/UnityPlayerActivity.java
@@ -0,0 +1,144 @@
+package com.RedFlag.ARVideoPlayback;
+
+import com.unity3d.player.*;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class UnityPlayerActivity extends Activity
+{
+    protected UnityPlayer mUnityPlayer; // don't change the name of this variable; referenced from native code
+
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+    }
+
+    // Setup activity layout
+    @Override protected void onCreate(Bundle savedInstanceState)
+    {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+
+        mUnityPlayer = new UnityPlayer(this);
+        setContentView(R.layout.unity_layout);
+        FrameLayout frameLayout = findViewById(R.id.unityPlayer);
+        frameLayout.addView(mUnityPlayer.getView());
+        mUnityPlayer.requestFocus();
+
+        Button stop = findViewById(R.id.stop);
+        Button start = findViewById(R.id.start);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mUnityPlayer.start();
+                mUnityPlayer.resume();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mUnityPlayer.quit();
+            }
+        });
+    }
+
+    @Override protected void onNewIntent(Intent intent)
+    {
+        // To support deep linking, we need to make sure that the client can get access to
+        // the last sent intent. The clients access this through a JNI api that allows them
+        // to get the intent set on launch. To update that after launch we have to manually
+        // replace the intent with the one caught here.
+        setIntent(intent);
+    }
+
+    // Quit Unity
+    @Override protected void onDestroy ()
+    {
+        mUnityPlayer.quit();
+        super.onDestroy();
+    }
+
+    // Pause Unity
+    @Override protected void onPause()
+    {
+        super.onPause();
+        mUnityPlayer.pause();
+    }
+
+    // Resume Unity
+    @Override protected void onResume()
+    {
+        super.onResume();
+
+    }
+
+    @Override protected void onStart()
+    {
+        super.onStart();
+
+    }
+
+    @Override protected void onStop()
+    {
+        super.onStop();
+        mUnityPlayer.stop();
+    }
+
+    // Low Memory Unity
+    @Override public void onLowMemory()
+    {
+        super.onLowMemory();
+        mUnityPlayer.lowMemory();
+    }
+
+    // Trim Memory Unity
+    @Override public void onTrimMemory(int level)
+    {
+        super.onTrimMemory(level);
+        if (level == TRIM_MEMORY_RUNNING_CRITICAL)
+        {
+            mUnityPlayer.lowMemory();
+        }
+    }
+
+    // This ensures the layout will be correct.
+    @Override public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        mUnityPlayer.configurationChanged(newConfig);
+    }
+
+    // Notify Unity of the focus change.
+    @Override public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        mUnityPlayer.windowFocusChanged(hasFocus);
+    }
+
+    // For some reason the multiple keyevent type is not supported by the ndk.
+    // Force event injection by overriding dispatchKeyEvent().
+    @Override public boolean dispatchKeyEvent(KeyEvent event)
+    {
+        if (event.getAction() == KeyEvent.ACTION_MULTIPLE)
+            return mUnityPlayer.injectEvent(event);
+        return super.dispatchKeyEvent(event);
+    }
+
+    // Pass any events not handled by (unfocused) views straight to UnityPlayer
+    @Override public boolean onKeyUp(int keyCode, KeyEvent event)     { return mUnityPlayer.injectEvent(event); }
+    @Override public boolean onKeyDown(int keyCode, KeyEvent event)   { return mUnityPlayer.injectEvent(event); }
+    @Override public boolean onTouchEvent(MotionEvent event)          { return mUnityPlayer.injectEvent(event); }
+    /*API12*/ public boolean onGenericMotionEvent(MotionEvent event)  { return mUnityPlayer.injectEvent(event); }
+}
